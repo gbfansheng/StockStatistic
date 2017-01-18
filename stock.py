288c7f42f1da5e0098734da3d450b392db835358
fanshenglin <fanshenglin001@163.com>
2017-01-18 19:12:01 +0800

    finish zhangting

--- stock.py
+++ stock.py
@@ -1,55 +1,75 @@
 # -*- coding: utf-8 -*-
+
 import pandas as pd
 import tushare as ts
 import os
 import datetime
 
+
 def downloadToday():
-	df = ts.get_today_all()
-	df.to_csv('today.csv',sep=',', encoding='utf-8')
-	pass
+    df = ts.get_today_all()
+    df.to_csv('today.csv', sep=',', encoding='utf-8')
+    pass
+
 
 def get_today():
-	todayDf = pd.read_csv('today.csv',sep=',', encoding='utf-8', dtype = str)
-	return todayDf
+    todayDf = pd.read_csv('today.csv', sep=',', encoding='utf-8', dtype=str)
+    return todayDf
+
+
+def get_data_path():
+    parent = os.path.dirname(os.getcwd())
+    data_path = parent + '/data/'
+    if not os.path.exists(data_path) :
+        os.makedirs(data_path)
+    return data_path
 
 def read(code):
-	fileName = code + '.csv'
-	df = pd.read_csv(fileName, sep=',', encoding='utf-8', dtype = str)
-	return df
+    fileName = code + '.csv'
+    # 文件是否存在
+    if os.path.isfile(get_data_path() + fileName):
+        df = pd.read_csv(fileName, sep=',', encoding='utf-8', dtype=str)
+        return df
+    else:
+        return None
+
+
+def save(df, code):
+    fileName = code + '.csv'
+    df.to_csv(get_data_path() + fileName)
+
 
 def downloadHistory():
-	todayDF = get_today()
-	codeList = list(todayDF['code'])
-	print(codeList)
-	for code in codeList:
-		fileName = code + '.csv'
-		print('fileName:' + fileName)
-		#文件是否存在
-		if os.path.isfile(fileName):
-			##处理时间
-			codeDF = read(code)
-			dateList = list(codeDF['date'])
-			dateString = dateList[0:1][0]
-			print('date:' + dateString)
-			nowdt = datetime.datetime.now()
-			nowdtstring = nowdt.strftime('%Y-%m-%d')
-			print('nowdtstring:' + nowdtstring)
-			if dateString == nowdtstring:
-				pass
-			else :
-				##获取两日日期之间的数据	
-				dt = datetime.datetime.strptime(dateString, '%Y-%m-%d')
-				dt = dt + datetime.timedelta(days = 1)
-				dtstring = dt.strftime('%Y-%m-%d') 
-				additionDF = ts.get_hist_data(code = code, start = dtstring, end = nowdtstring)
-				concatDF = additionDF.append(codeDF)
-				concatDF.to_csv(code + '.csv')
-		else:
-			df = ts.get_hist_data(code)
-			df.to_csv(code + '.csv')
+    todayDF = get_today()
+    codeList = list(todayDF['code'])
+    for code in codeList:
+        codeDF = read(code)
+        if codeDF is None:
+            print('downloading ' + code)
+            df = ts.get_hist_data(code)
+            if df is None:
+                pass
+            else:
+                # 新股没有数据，return none
+                save(df, code)
+        else:
+            ##处理时间
+            dateList = list(codeDF['date'])
+            dateString = dateList[0:1][0]
+            nowdt = datetime.datetime.now()
+            nowdtstring = nowdt.strftime('%Y-%m-%d')
+            if dateString == nowdtstring:
+                pass
+            else:
+                ##获取两日日期之间的数据
+                dt = datetime.datetime.strptime(dateString, '%Y-%m-%d')
+                dt = dt + datetime.timedelta(days=1)
+                dtstring = dt.strftime('%Y-%m-%d')
+                additionDF = ts.get_hist_data(code=code, start=dtstring, end=nowdtstring)
+                concatDF = additionDF.append(codeDF)
+                save(concatDF, code)
 
 
-if __name__=='__main__':   
-	downloadToday()
-	downloadHistory()
+if __name__ == '__main__':
+    downloadToday()
+    downloadHistory()
--- zhangting.py
+++ zhangting.py
@@ -1,47 +1,64 @@
 # -*- coding: utf-8 -*-
+
 import pandas as pd
+import pickle
 import stock
 import datetime
+import os
+import stock
 
 def processDf(code, df):
-	global dateDict
-	# begin = datetime.date(2014,1,19)  
-    end = datetime.datetime.now()
-    for i in range((end - begin).days+1):  
-        day = begin + datetime.timedelta(days=i)  
-        print str(day) 
-		daystring = day.strftime('%Y-%m-%d')
-		dfForDay = df[df['date'].isin([daystring])]
-		datelist = dateDict[daystring]
-		
-		if datelist:
-			pass
-		else :
-			datelist = []
+    global dateDict
+    begin = datetime.date(2016, 12, 30)
+    end = datetime.datetime.now().date()
+    for i in range((end - begin).days + 1):
+        day = begin + datetime.timedelta(days=i)
+        daystring = day.strftime('%Y-%m-%d')
+        dfForDay = df[df['date'].isin([daystring])]
+        datelist = dateDict.get(daystring, [])
 
-		#当天有数据
-		if dfForDay:
-			p_change = dfForDay['p_change'][0]
-			high = dfForDay['high'][0]
-			close = dfForDay[close][0]
-			if float(p_change) > 9.9 and float(high) == float(close):
-				#如果涨停则保存
-				print('append')
-				datelist.append(code)
-				dateDict[daystring] = datelist
+        # 当天有数据
+        if not dfForDay.empty:
+            p_change = float(dfForDay['p_change'])
+            high = float(dfForDay['high'])
+            close = float(dfForDay['close'])
+            turnover = float(dfForDay['turnover'])
+            if p_change > 9.9 and p_change < 11 and high == close and turnover > 3.0:
+                # 如果涨停则保存
+                datelist.append(code)
+                dateDict[daystring] = datelist
+
+
+def saveDict():
+    global dateDict
+    f = open(stock.get_data_path() + 'zhangting','w')
+    pickle.dump(dateDict,f)
+    f.close()
+
+
+def getDict():
+    f = open(stock.get_data_path() + 'zhangting','r')
+    dateDict = pickle.load(f)
+    return dateDict
 
 
 def zhangting():
-	global dateDict
-	# todayDF = stock.get_today()
-	# codeList = list(todayDF['code'])
-	# for code in codeList:
-		code = '300505'
-		codeDF = stock.read(code)
-		#文件是否存在
-		if codeDF:
-			processDf(code, codeDF)
-	print(dateDict)
+    global dateDict
+    dateDict = {}
+    todayDF = stock.get_today()
+    codeList = list(todayDF['code'])
+    for code in codeList:
+        print (code)
+        codeDF = stock.read(code)
+        # 文件是否存在
+        if codeDF is None:
+            pass
+        else:
+            processDf(code, codeDF)
+    saveDict()
 
-if __name__=='__main__':   
-	zhangting()
+
+
+if __name__ == '__main__':
+    zhangting()
+    getDict()
